1cf6740cf6c643ff175ae1245bac22b3ed7ea543
Jesse-Leegwater <Jleegwater@live.com>
2021-09-07 20:45:20 -0700

    Finished MVP

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,86 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+const Car = require("./cars-model");
+const db = require("../../data/db-config");
+const vinValidator = require("vin-validator");
+
+const checkCarId = async (req, res, next) => {
+  const car = await Car.getById(req.params.id);
+  if (!car) {
+    next({
+      message: `car with id ${req.params.id} is not found`,
+      status: 404,
+    });
+  } else {
+    req.car = car;
+    next();
+  }
+};
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  try {
+    const { vin, make, model, mileage } = req.body;
+    if (!vin) {
+      next({
+        message: `vin is missing`,
+        status: 400,
+      });
+    }
+    if (!make) {
+      next({
+        message: `make is missing`,
+        status: 400,
+      });
+    }
+    if (!model) {
+      next({
+        message: `model is missing`,
+        status: 400,
+      });
+    }
+    if (!mileage) {
+      next({
+        message: `mileage is missing`,
+        status: 400,
+      });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
 
-const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+const checkVinNumberValid = async (req, res, next) => {
+  try {
+    const isValid = vinValidator.validate(req.body.vin);
+    if (isValid) {
+      next();
+    } else {
+      next({
+        message: `vin ${req.body.vin} is invalid`,
+        status: 400,
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
 
-const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+const checkVinNumberUnique = async (req, res, next) => {
+  const { vin } = req.body;
+  const foundVin = await db("cars").where("vin", vin).first();
+  if (foundVin) {
+    next({
+      message: `vin ${vin} already exists`,
+      status: 400,
+    });
+  } else {
+    next();
+  }
+};
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberUnique,
+  checkVinNumberValid,
+};
--- api/server.js
+++ api/server.js
@@ -1,7 +1,21 @@
-const express = require("express")
+const express = require("express");
+//const helmet = require("helmet");
 
-const server = express()
+const carsRouter = require("./cars/cars-router");
 
-// DO YOUR MAGIC
+const server = express();
 
-module.exports = server
+//server.use(helmet());
+server.use(express.json());
+
+server.use("/api/cars", carsRouter);
+
+server.use((err, req, res, next) => {
+  // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
